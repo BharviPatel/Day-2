fb5e1658effa40aafe2fdcf5fe4993cad64d3d0d
Bharvi patel <84793346+BharviPatel@users.noreply.github.com>
2021-05-26 21:37:32 +0530

    Add files via upload

---
+++ addition.php
@@ -0,0 +1,8 @@
+<?php
+$a = 10;
+$b = 20;
+echo "A value is ".$a;
+echo "<br/>B value is ".$b;
+//echo "<br/> A value is $a<br/>B value is $b";
+echo "<br/>A value is $a<br/>B value is $b<br/> Sum is ".($a+$b);
+?>
